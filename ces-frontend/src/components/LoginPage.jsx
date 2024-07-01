cd2b315eac2aeb06f74dc38d30fe78c2052429e8
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-07-01 16:17:31 +0800

    Update LoginPage.jsx

--- ces-frontend/src/components/LoginPage.jsx
+++ ces-frontend/src/components/LoginPage.jsx
@@ -15,6 +15,7 @@ function LoginPage(){
                     <Form.Group className='mb-3' controlId='LogPass'>
                         <Form.Label>Password</Form.Label>
                         <Form.Control className='input' type='password' placeholder='Insert your Password here'/>
+                        <button></button>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="CheckBox">

375d27f1889355e048c1b83f7a2fd2a55df22e3e
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-07-01 11:09:28 +0800

    Bootstrap and login

---
+++ ces-frontend/src/components/LoginPage.jsx
@@ -0,0 +1,13 @@
+
+
+function LoginPage(){
+    return(
+        <body  className="bg">
+            <div>
+            Hello World
+            </div>
+        </body>
+    );
+}
+
+export default LoginPage;

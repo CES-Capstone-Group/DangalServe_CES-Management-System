605989b6757ac9dfb17074e1c8621e83d4c46010
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-07-01 16:17:24 +0800

    Added login form

--- ces-frontend/src/components/LoginPage.jsx
+++ ces-frontend/src/components/LoginPage.jsx
@@ -1,11 +1,32 @@
-
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function LoginPage(){
     return(
-        <body  className="bg">
-            <div>
-            Hello World
-            </div>
+        <body className="bg">
+            <Card>
+                <Form>
+                    <Form.Group className='mb-3' controlId='LogUsername'>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control className='input'  type='text' placeholder='Insert your username here'/>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='LogPass'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control className='input' type='password' placeholder='Insert your Password here'/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="CheckBox">
+                        <Form.Check type="checkbox" label="Remember Me" />
+                    </Form.Group>
+
+                    <Form.Group className='d-grid gap-2'>
+                        <Button controlId='button' variant='success' type='submit' size='lg'>Submit</Button>
+                    </Form.Group>
+                    
+                </Form>
+            </Card>
         </body>
     );
 }

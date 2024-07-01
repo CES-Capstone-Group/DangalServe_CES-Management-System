c78f8b7255d6e5cc4d452ee9e873daf7a58a5635
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-07-01 17:32:30 +0800

    update

--- ces-frontend/src/components/LoginPage.jsx
+++ ces-frontend/src/components/LoginPage.jsx
@@ -9,21 +9,21 @@ import Logo from '../assets/pnclogo.png'
 function LoginPage(){
     return(
         <body className="bg">
-            <Card>
-                <Card.Img variant='top' src={Logo}/>
-                <Card.Title style={{textAlign: 'center'}}>Community Extension Service Management System</Card.Title><br/><br/>
+            <Card style={{width: '900px'}}>
+                <Card.Img style={{width: '35rem', marginLeft: '6rem'}} variant='top' src={Logo}/>
+                <Card.Title style={{textAlign: 'center'}}>Community Extension Service Management System</Card.Title><br/>
                 <Form>
                     <Form.Group className='mb-3' controlId='LogUsername'>
-                        <Form.Label>Username</Form.Label>
+                        <Form.Label className='h5'>Username</Form.Label>
                         <InputGroup>
                             <Form.Control className='input'  type='text' placeholder='Insert your username here'/>
                         </InputGroup>
                     </Form.Group>
 
                     <Form.Group className='mb-3' controlId='LogPass'>
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label className='h5'>Password</Form.Label>
                         <InputGroup>
-                            <Form.Control className='input' type='password' placeholder='Insert your Password here'/>
+                            <Form.Control className='input ' type='password' placeholder='Insert your Password here'/>
                             <Button><FontAwesomeIcon icon="fa-solid fa-eye" /></Button>
                         </InputGroup>
                     </Form.Group>
@@ -33,7 +33,7 @@ function LoginPage(){
                     </Form.Group>
 
                     <Form.Group className='d-grid gap-2'>
-                        <Button controlId='button' variant='success' type='submit' size='lg'>Submit</Button>
+                        <Button style={{marginTop: '2rem'}} controlId='button' variant='success' type='submit' size='lg'>Submit</Button>
                     </Form.Group>
                     
                 </Form>

37a9ff77263dbd06e4c71d0ecab204dc2e150d27
CancisioKarl <cancisiokarlangelo76@gmail.com>
2024-07-16 16:14:26 +0800

    update

--- ces-frontend/src/App.jsx
+++ ces-frontend/src/App.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import TopNav from './components/TopNav';
 import BrgySidebar from './components/BrgySidebar';
 import MainContent from './components/MainContent';
-import AdminMainContent from './components/AdminMainContent';
-import AdminSidebar from './components/AdminSidebar';
 import { Container, Row, Col, } from 'react-bootstrap';
 import CoorSidebar from './components/CoorSidebar';
+import UserManagementCon from './components/UserManagementCon';
+import BrgyProposalPage from './components/BrgyProposalPage';
+import AdminSidebar from './components/AdminSidebar';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 const App = () => {
   return (
     <div>
       <Container fluid>
         <Row>
-          <Col md={1} lg={2} style={{padding: '0px', boxShadow: '20px'}}>
-            <AdminSidebar />
+          <Col md={1} lg={2} style={{padding: '0px'}}>
+            <AdminSidebar/>
           </Col>
-          <Col md={9} lg={10} className="maincon ms-sm-auto px-md-0">
+          <Col md={9} lg={10} className="p-0 m-0">
             <TopNav/><br /><br />
-            <AdminMainContent />
+            <UserManagementCon />
           </Col>
         </Row>
       </Container>

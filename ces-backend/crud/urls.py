a85919c12e6a1124d29edb50e1a4538c1344ff1b
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-11-15 00:02:44 +0800

    Update urls.py

--- ces-backend/crud/urls.py
+++ ces-backend/crud/urls.py
@@ -19,14 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to the Homepage!")
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('', lambda request: redirect('api/', permanent=True)),  # Redirect to 'api/'
     path('api/', include('api.urls')),
     path('impact/', include('impact.urls')),
-    
-    # path('evaluation/', include('evaluation.urls'))
     path('evaluation/', include('evaluation.urls')),
+    path('media/<path:path>/', include('django.views.static.serve')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

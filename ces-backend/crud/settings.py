bba2fcd6e7fb21a069decc6a01304a07fdd430e2
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-11-14 21:34:34 +0800

    Update settings.py

--- ces-backend/crud/settings.py
+++ ces-backend/crud/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ces_db',
         'USER': 'root',
-        'PASSWORD': 'Bellosql27!',
+        'PASSWORD': 'Cloud5024',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306'      # or the port on which your MySQL server is listening
     }

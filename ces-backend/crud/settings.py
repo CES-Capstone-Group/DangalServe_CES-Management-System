4e504a6e85b2da73d448d8723929a25a535836dc
BlankPhantom <123438824+BlankPhantom@users.noreply.github.com>
2024-11-17 00:47:55 +0800

    Update settings.py

--- ces-backend/crud/settings.py
+++ ces-backend/crud/settings.py
@@ -110,28 +110,28 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# Local Database for Debugging locally
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ces_db',
-        'USER': 'root',
-        'PASSWORD': 'Cloud5024',
-        'HOST': 'localhost',  # or the hostname where your MySQL server is running
-        'PORT': '3306'      # or the port on which your MySQL server is listening
-    }
-}
-
+# # Local Database for Debugging locally
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'PamantasanCES$ces_db',
-#         'USER': 'PamantasanCES',
+#         'NAME': 'ces_db',
+#         'USER': 'root',
 #         'PASSWORD': 'Cloud5024',
-#         'HOST': 'PamantasanCES.mysql.pythonanywhere-services.com',  # or the hostname where your MySQL server is running
+#         'HOST': 'localhost',  # or the hostname where your MySQL server is running
 #         'PORT': '3306'      # or the port on which your MySQL server is listening
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PamantasanCES$ces_db',
+        'USER': 'PamantasanCES',
+        'PASSWORD': 'Cloud5024',
+        'HOST': 'PamantasanCES.mysql.pythonanywhere-services.com',  # or the hostname where your MySQL server is running
+        'PORT': '3306'      # or the port on which your MySQL server is listening
+    }
+}
 
 
 # Password validation
